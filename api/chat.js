1d9216cf94f461f132633f2f4024cde4d0a6bf26
On One Tutor <78573591+youryourwooops@users.noreply.github.com>
2025-04-17 20:12:43 +0200

    Update chat.js

--- api/chat.js
+++ api/chat.js
@@ -1,7 +1,7 @@
 export default async function handler(req, res) {
   const { message } = req.body;
 
-  const response = await fetch("https://xiaoai.plus/v1", {
+  const response = await fetch("https://xiaoai.plus", {
     method: "POST",
     headers: {
       "Content-Type": "application/json",
